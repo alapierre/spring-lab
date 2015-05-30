fa8e0e7e5de66f335e32afd95e6bbf834f625453
CSI <CSI@CSI-Komputer>
2015-05-30 15:12:10 +0200

    convertert

---
+++ Spring-lab1_05/src/main/java/pl/com/softproject/spring/converter/PersonConverter.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.softproject.spring.converter;
+
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+import pl.com.softproject.spring.model.Person;
+
+/**
+ *
+ * @author CSI
+ */
+
+public class PersonConverter implements Converter<String, Person> {
+
+    @Override
+    public Person convert(String s) {
+        Person p = new Person();
+
+        String[] res = s.split(";");
+
+        p.setId(Long.parseLong(res[0]));
+        p.setName(res[1]);
+        p.setSurname(res[2]);
+
+        return p;
+    }
+
+}
--- Spring-lab1_05/src/main/java/pl/com/softproject/spring/model/editors/AddressEditor.java
+++ Spring-lab1_05/src/main/java/pl/com/softproject/spring/model/editors/AddressEditor.java
@@ -1,14 +1,25 @@
 package pl.com.softproject.spring.model.editors;
 
 import java.beans.PropertyEditorSupport;
+import pl.com.softproject.spring.model.Address;
 
 public class AddressEditor extends PropertyEditorSupport {
-
-	@Override
-	public void setAsText( String text ) throws IllegalArgumentException {
-		//Zaimplementuj metode. Przeksztalc lancuch znakow na obiekt Address
-	}
-	
-	
+    
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        
+        Address address = new Address();
+        
+        String[] res = text.split(";");
+        
+        address.setId(Long.parseLong(res[0]));
+        address.setStreet(res[1]);
+        address.setNumber(res[2]);
+        address.setPostalCode(res[3]);
+        address.setCity(res[4]);
+        
+        setValue(address);
+        
+    }
+    
 }
-
