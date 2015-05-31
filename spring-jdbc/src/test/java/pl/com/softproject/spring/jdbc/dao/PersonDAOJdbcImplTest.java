62029bfe1795150b72c08907abc67a146c387a7d
CSI <CSI@CSI-Komputer>
2015-05-31 11:21:01 +0200

    select na Person

---
+++ spring-jdbc/src/test/java/pl/com/softproject/spring/jdbc/dao/PersonDAOJdbcImplTest.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.softproject.spring.jdbc.dao;
+
+import java.util.List;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import pl.com.softproject.spring.jdbc.model.Person;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+/**
+ *
+ * @author CSI
+ */
+@ContextConfiguration("/context.xml")
+@RunWith(SpringJUnit4ClassRunner.class)
+public class PersonDAOJdbcImplTest {
+    
+    @Autowired
+    private PersonDAO personDAO;
+    
+    /**
+     * Test of loadById method, of class PersonDAOJdbcImpl.
+     */
+    @Test
+    public void testLoadById() {
+        System.out.println("loadById");
+        
+        Person person = personDAO.loadById(1);
+        
+        System.out.println(person);
+        
+//        assertEquals("niepoprawne imię", "Adam", p.getName());
+//        assertEquals("niepoprawne id", 1L, (long)p.getId());
+        
+        assertThat(person.getName(), equalToIgnoringCase("adam"));
+        
+    }
+
+    /**
+     * Test of loadAll method, of class PersonDAOJdbcImpl.
+     */
+    @Test
+    public void testLoadAll() {
+        
+        System.out.println("loadAll");
+        
+        List<Person> res = personDAO.loadAll();
+        
+        System.out.println(res);
+        
+        assertThat(res.size(), is(equalTo(2)));
+        
+    }
+    
+    
+    
+}
