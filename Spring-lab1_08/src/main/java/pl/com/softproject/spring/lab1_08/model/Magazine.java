7af7fc42692de209be3fe9a08fe241eef54e38c2
S108_8 <S108_8@S108_8-Komputer>
2015-06-13 09:46:32 +0200

    ppp

---
+++ Spring-lab1_08/src/main/java/pl/com/softproject/spring/lab1_08/model/Magazine.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.softproject.spring.lab1_08.model;
+
+/**
+ *
+ * @author S108_8
+ */
+public class Magazine {
+    
+//    private Product id_product;
+    private Long count;
+
+//    public Product getId_product() {
+//        return id_product;
+//    }
+
+    public Long getCount() {
+        return count;
+    }
+
+//    public void setId_product(Product id_product) {
+//        this.id_product = id_product;
+//    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+    
+}
