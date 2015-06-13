3014144a30862cbdfafa9c4735018eb5277418a8
agata.kolodynska <S108_5@S108_5-Komputer>
2015-06-13 09:40:27 +0200

    Adres i Kategoria

---
+++ Spring-lab1_08/src/main/java/pl/com/softproject/spring/lab1_08/model/Category.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pl.com.softproject.spring.lab1_08.model;
+
+/**
+ *
+ * @author S108_5
+ */
+public class Category {
+    
+    private String name;
+    private Category parent;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getParent() {
+        return parent;
+    }
+
+    public void setParent(Category parent) {
+        this.parent = parent;
+    }
+    
+}
