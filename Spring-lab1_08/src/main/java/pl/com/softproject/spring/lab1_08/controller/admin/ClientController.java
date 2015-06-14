3e730f889ff8097e8aa972a707a345d538e3fb6e
Piotr Szwed <szwed.piotr@gmail.com>
2015-06-14 12:07:47 +0200

    poprawka

--- Spring-lab1_08/src/main/java/pl/com/softproject/spring/lab1_08/controller/admin/ClientController.java
+++ Spring-lab1_08/src/main/java/pl/com/softproject/spring/lab1_08/controller/admin/ClientController.java
@@ -28,83 +28,89 @@ import pl.com.softproject.spring.lab1_08.model.CustommerDto;
 @Controller
 @RequestMapping("/admin/client")
 public class ClientController {
-    
+
     @Autowired
     private ClientDAO clientDAO;
-    
+
     @Autowired
     private AddressDAO addressDAO;
-    
+
     @RequestMapping(value = "/add", method = RequestMethod.GET)
     public ModelAndView add() {
-        
+
         ModelAndView model = new ModelAndView("edit_client");
         model.addObject("client", new CustommerDto());
-        
+
         return model;
     }
-    
+
     @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
     public ModelAndView edit(@PathVariable(value = "id") Long id) {
-        
+
         Client editClient = clientDAO.findOne(id);
-        
-        Set<Address> ad =  editClient.getAddresses();
-        
-        Address editAddress = addressDAO.findOne(ad.iterator().next().getId().toString());
-        
-        CustommerDto cd = new CustommerDto();
-        cd.setName(editClient.getName());
-        cd.setLastName(editClient.getLastName());
-        cd.setStreet(editAddress.getStreet());
-        cd.setCity(editAddress.getCity());
-        cd.setPostCode(editAddress.getPostCode());
+
+        Set<Address> ad = editClient.getAddresses();
         
         ModelAndView model = new ModelAndView("edit_client");
-        model.addObject("client", cd);
-        
+
+
+        if (!ad.isEmpty()) {
+            CustommerDto cd = new CustommerDto();
+            cd.setName(editClient.getName());
+            cd.setLastName(editClient.getLastName());
+            cd.setStreet(ad.iterator().next().getStreet());
+            cd.setCity(ad.iterator().next().getCity());
+            cd.setPostCode(ad.iterator().next().getPostCode());
+
+            model.addObject("client", cd);
+        } else {
+            model.addObject("client", new CustommerDto());
+        }
+
         return model;
     }
-    
+
     @RequestMapping(value = "/save", method = RequestMethod.POST)
     public ModelAndView save(@Valid CustommerDto client, BindingResult br) {
-        
+
         System.out.println(client);
-        
-        if(br.hasErrors()) {
-            
+
+        if (br.hasErrors()) {
+
             System.out.println(br.getAllErrors());
-            
+
             ModelAndView m = new ModelAndView("edit_client");
             m.addObject("client", client);
             return m;
         }
-        
+
         Client c = new Client();
-        
+
         Address address = new Address();
         address.setCity(client.getCity());
         address.setPostCode(client.getPostCode());
         address.setStreet(client.getStreet());
         address.setType(Address.AddressType.HOME);
-        
+
         c.getAddresses().add(address);
         c.setName(client.getName());
         c.setLastName(client.getLastName());
-        
+
         address.setClient(c);
-        
+
         clientDAO.save(c);
         
+        addressDAO.save(address);
+
         return new ModelAndView("redirect:list");
     }
-    
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
-        
+
         model.addAttribute("clients", clientDAO.findAll());
-        
+
         return "client-list";
-        
+
     }
 }
