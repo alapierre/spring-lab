04021e14e05cd35f4430f794543339758d30a8ae
adrian <adrian@soft-project.pl>
2015-06-19 19:48:35 +0200

    Spring WF - init

---
+++ spring-webflow/src/main/java/pl/com/softproject/spring/ws/model/Person.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2015-05-26 the original author or authors.
+ */
+package pl.com.softproject.spring.ws.model;
+
+/**
+ *
+ * @author Adrian Lapierre <adrian@soft-project.pl>
+ */
+public class Person {
+    
+    private String name;
+    private String lastName;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    
+}
---
+++ spring-webflow/src/main/webapp/WEB-INF/flows/helloWorld.jsp
@@ -0,0 +1,10 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" 
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html>
+	<head>
+		<title>Witaj, świecie!</title>
+	</head>
+	<body>
+	<h2>Witaj, świecie Spring SWF!</h2>		
+	</body>
+</html>
